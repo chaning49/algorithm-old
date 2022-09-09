8a337f39dabc0e76442c6b15edc3617ff16571ea
chaning49 <ggammancj@gmail.com>
2022-09-09 20:57:35 +0900

    BOJ 8958 OX퀴즈

---
+++ BOJ/8958.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            int sum = 0;
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O') cnt++;
+                else cnt = 0;
+                sum += cnt;
+            }
+            System.out.println(sum);
+        }
+    }
+}
