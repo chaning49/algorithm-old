f643b0f57b98eb5bbccbf7528313d979afb152d1
chaning49 <ggammancj@gmail.com>
2022-09-17 04:08:58 +0900

    Programmers 이진 변환 반복하기

---
+++ Programmers/IterBinChange.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] ans = new int[2];
+        while (s.length() > 1) {
+            int cnt = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') ans[1]++;
+                else cnt++;
+            }
+
+            s = Integer.toBinaryString(cnt);
+            ans[0]++;
+        }
+        return ans;
+    }
+}
