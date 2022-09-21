db549ae54ce68c34e02ae8d98fd8cae6b75efd87
chaning49 <ggammancj@gmail.com>
2022-09-21 22:48:35 +0900

    BOJ 2292 벌집

---
+++ BOJ/기본 수학 1/2292.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+집
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        int limits = 1;
+
+        // 벌집은 1, 2~7, 8~19 구간에서 6의 배수만큼 늘어난다.
+        // n의 값이 들어왔을 때, 특정 범위 안에 속하면 최소 방문수는 그 범위 내에서 같다.
+        while (n > limits) {
+            limits += 6 * cnt;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
