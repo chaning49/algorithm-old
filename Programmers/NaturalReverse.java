26c1122c4eb67ba1deb2dc35d5c5fe9398b62f57
chaning49 <ggammancj@gmail.com>
2022-10-02 20:17:53 +0900

    Programmers 자연수 뒤집어 배열로 만들기

---
+++ Programmers/NaturalReverse.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(long n) {
+        String[] temp = Long.toString(n).split("");
+        int[] answer = new int[temp.length];
+        for (int i = 0; i < temp.length; i++) {
+            answer[i] = Integer.parseInt(temp[temp.length - i - 1]);
+        }
+        return answer;
+    }
+}
