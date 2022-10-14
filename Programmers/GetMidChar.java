0dd2d96612fbeb02ab3e455d5a874ba0c209d433
chaning49 <79316402+chaning49@users.noreply.github.com>
2022-10-14 21:13:11 +0900

    가운데 글자 가져오기

---
+++ Programmers/GetMidChar.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        // s의 길이를 반으로 나눈 수 저장
+        int half = s.length() / 2;
+        
+        // s가 홀수인지 짝수인지 판별, 짝수라면 chk == true
+        if (s.length() % 2 != 0) {
+            answer += s.charAt(half);
+        }
+        else {
+            answer += s.charAt(half - 1);
+            answer += s.charAt(half);
+        }
+        
+        return answer;
+    }
+}
