9f7b7a61d3028b63418e1d074e8b967401b60bb0
chaning49 <ggammancj@gmail.com>
2022-11-12 23:18:58 +0900

    Programmers 문자열 계산하기

--- Programmers/StrCalculate.java
+++ Programmers/StrCalculate.java
@@ -8,7 +8,7 @@ class Solution {
         // 첫번째 값을 저장
         int answer = Integer.parseInt(strArr[0]);
 
-        // 연산자만 체크해서 다음 값을 더해준다.
+        // 연산자만 체크해서 다음 값을 더하거나 빼준다.
         for (int i = 1; i < strArr.length; i += 2) {
             if (strArr[i].equals("+")) answer += Integer.parseInt(strArr[i + 1]);
             if (strArr[i].equals("-")) answer -= Integer.parseInt(strArr[i + 1]);
