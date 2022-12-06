405254f4a6dc9de46a276cc58eabee4c96e0e067
chaning49 <ggammancj@gmail.com>
2022-12-06 23:46:07 +0900

    Programmers A로 B 만들기(코드 추가)

--- Programmers/MakeEqual.java
+++ Programmers/MakeEqual.java
@@ -1,3 +1,4 @@
+// 1. Char[] 사용
 import java.util.*;
 
 class Solution {
@@ -15,6 +16,31 @@ class Solution {
         // String으로 붙여준다.
         String strBf = new String(charBf);
         String strAf = new String(charAf);
+
+        // 비교해서 같으면 1, 다르면 0
+        if (strBf.equals(strAf)) return 1;
+        else return 0;
+    }
+}
+
+// 2. String[] 사용
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+
+        // split으로 단어 하나씩 String[]에 저장
+        String[] arrBf = before.split("");
+        String[] arrAf = after.split("");
+
+        // 오름차순으로 똑같이 정렬
+        Arrays.sort(arrBf);
+        Arrays.sort(arrAf);
+
+        // String으로 붙여준다.
+        String strBf = Arrays.toString(arrBf);
+        String strAf = Arrays.toString(arrAf);
 
         // 비교해서 같으면 1, 다르면 0
         if (strBf.equals(strAf)) return 1;
