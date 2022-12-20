93f7dee698a2336319a15299567d20a5dcbe5dd0
chaning49 <ggammancj@gmail.com>
2022-12-21 00:32:39 +0900

    Programmers 햄버거 만들기

--- Programmers/MakeBurger.java
+++ Programmers/MakeBurger.java
@@ -1,3 +1,31 @@
+// 내가 짠 코드
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        Stack<Integer> stack = new Stack<>();
+        int cnt = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack.push(i);
+            cnt++;
+            if (cnt >= 4 && stack.get(cnt - 1) == 1
+                    && stack.get(cnt - 2) == 3
+                    && stack.get(cnt - 3) == 2
+                    && stack.get(cnt - 4) == 1) {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                cnt -= 4;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+// 좋은 코드
 class Solution {
     public int solution(int[] ingredient) {
         int[] stack = new int[ingredient.length];
