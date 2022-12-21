d2cd6f56ed6a8f1b421a639079a7034c91f9ad0d
chaning49 <ggammancj@gmail.com>
2022-12-21 20:52:48 +0900

    Programmers 옹알이(2)

---
+++ Programmers/babbling2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for (String b : babbling) {
+            if (b.contains("ayaaya") || b.contains("yeye") || b.contains("woowoo") || b.contains("mama")) continue;
+            b = b.replace("aya", "-");
+            b = b.replace("ye", "-");
+            b = b.replace("woo", "-");
+            b = b.replace("ma", "-");
+            b = b.replace("-", "");
+
+            if (b.equals("")) answer++;
+        }
+
+        return answer;
+    }
+}
